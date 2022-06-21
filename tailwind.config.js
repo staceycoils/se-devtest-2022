dfbd57375895b1fb453182a91d0a79d02be6194f
Stacey <staceyelsom@gmail.com>
2022-06-21 14:05:48 +0100

    Added Graphik font

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ content: [
         'custom-black': '#181818',
         'custom-lilac': '#4452FE',
         'custom-grey': '#1E1E1E',
-      }
+      },
+      fontFamily: {
+        'graphik': ['Graphik']
+      },
     },
   },
   plugins: [],
